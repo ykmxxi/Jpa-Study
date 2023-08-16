c40daf58fb30bd77a87b34d2b1b0369f7e51156b
ykmxxi <ykmxxi97@gmail.com>
2023-08-16 16:30:11 +0900

    Implement #3- delivery entity

---
+++ jpashop/src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -0,0 +1,33 @@
+package jpabook.jpashop.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter
+public class Delivery {
+
+	@Id @GeneratedValue
+	@Column(name = "delivery_id")
+	private Long id;
+
+	@OneToOne(mappedBy = "delivery", fetch = FetchType.LAZY)
+	private Order order;
+
+	@Embedded
+	private Address address;
+
+	@Enumerated(EnumType.STRING) // EnumType.ORDINAL을 사용하면 중간에 값이 추가되면 장애 발생
+	private DeliveryStatus status; // READY, COMP
+
+}
