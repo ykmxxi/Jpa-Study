9fd1f156514b9ae99883af06feb076a300210e5c
ykmxxi <ykmxxi97@gmail.com>
2023-08-16 16:30:57 +0900

    Implement #3- address embedded type

---
+++ jpashop/src/main/java/jpabook/jpashop/domain/Address.java
@@ -0,0 +1,25 @@
+package jpabook.jpashop.domain;
+
+import javax.persistence.Embeddable;
+
+import lombok.Getter;
+
+@Embeddable
+@Getter
+public class Address {
+
+	private String city;
+	private String street;
+	private String zipcode;
+
+	// JPA 스펙 상 기본 생성자가 필요
+	protected Address() {
+	}
+
+	public Address(String city, String street, String zipcode) {
+		this.city = city;
+		this.street = street;
+		this.zipcode = zipcode;
+	}
+
+}
