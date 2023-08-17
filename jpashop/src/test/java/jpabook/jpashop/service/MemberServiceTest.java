4d4fc79aa7b08ea3b188153448122260b38dd494
ykmxxi <ykmxxi97@gmail.com>
2023-08-17 14:50:43 +0900

    Test #5- member service test
    - 회원 가입 성공 테스트
    - 중복 회원 예외처리 테스트

---
+++ jpashop/src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -0,0 +1,56 @@
+package jpabook.jpashop.service;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.transaction.annotation.Transactional;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@Transactional
+class MemberServiceTest {
+
+	@Autowired MemberService memberService;
+	@Autowired MemberRepository memberRepository;
+
+	@Test
+	@DisplayName("회원 가입 성공 테스트")
+	void join() {
+		// given
+		Member member = new Member();
+		member.setName("kim");
+
+		// when
+		Long savedId = memberService.join(member);
+
+		// then
+		assertEquals(member, memberRepository.findOne(savedId));
+	}
+
+	@Test
+	@DisplayName("중복된 이름이 존재하면 회원 가입 예외처리 테스트")
+	void duplicateMember() {
+		// given
+		Member member1 = new Member();
+		member1.setName("kim");
+
+		Member member2 = new Member();
+		member2.setName("kim");
+
+		// when & then
+		assertThatThrownBy(() -> {
+			memberService.join(member1);
+			memberService.join(member2);
+		}).isInstanceOf(IllegalStateException.class);
+	}
+
+}
