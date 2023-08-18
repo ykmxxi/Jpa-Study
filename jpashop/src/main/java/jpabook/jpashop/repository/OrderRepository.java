43229d128ccf7e049bb37d4ae2a881471a6c1eb4
ykmxxi <ykmxxi97@gmail.com>
2023-08-18 14:32:12 +0900

    Implement #9- find all order by jpql
    - JPQL을 사용한 동적 쿼리 작성

--- jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -1,10 +1,15 @@
 package jpabook.jpashop.repository;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
 
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
 
 import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderSearch;
 import lombok.RequiredArgsConstructor;
 
 @Repository
@@ -21,7 +26,45 @@ public class OrderRepository {
 		return em.find(Order.class, id);
 	}
 
-	// TODO: 전체 주문 검색 기능 개발
-	// public List<Order> findAll(OrderSearch orderSearch) {}
+	/**
+	 * 동적 쿼리: JPQL 사용
+	 */
+	public List<Order> findAllByString(OrderSearch orderSearch) {
+		String jpql = "select o from Order o join o.member";
+		boolean isFirstCondition = true;
+
+		// 주문 상태 검색
+		if (orderSearch.getOrderStatus() != null) {
+			if (isFirstCondition) {
+				jpql += " where";
+				isFirstCondition = false;
+			} else {
+				jpql += " and";
+			}
+			jpql += " o.status = :status";
+		}
+
+		// 회원 이름 검색
+		if (StringUtils.hasText(orderSearch.getMemberName())) {
+			if (isFirstCondition) {
+				jpql += " where";
+				isFirstCondition = false;
+			} else {
+				jpql += " and";
+			}
+			jpql += " m.name like :name";
+		}
+
+		TypedQuery<Order> query = em.createQuery(jpql, Order.class)
+			.setMaxResults(1000);
+		if (orderSearch.getOrderStatus() != null) {
+			query = query.setParameter("status", orderSearch.getOrderStatus());
+		}
+		if (StringUtils.hasText(orderSearch.getMemberName())) {
+			query = query.setParameter("name", orderSearch.getMemberName());
+		}
+
+		return query.getResultList();
+	}
 
 }
