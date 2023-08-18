946c95a9e2eda2677088f5c1e36d03767fe39c7e
ykmxxi <ykmxxi97@gmail.com>
2023-08-18 10:31:12 +0900

    Implement #7- item business logic
    - 재고 증가 비즈니스 로직
    - 재고 감소 비즈니스 로직

--- jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -13,6 +13,7 @@ import javax.persistence.InheritanceType;
 import javax.persistence.ManyToMany;
 
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -32,5 +33,27 @@ public abstract class Item {
 
 	@ManyToMany(mappedBy = "items")
 	private List<Category> categories = new ArrayList<>();
+
+	//== 비즈니스 로직 ==//
+	// DDD에서 엔티티 자체가 해결할 수 있는 것은 엔티티 안에 비즈니스 로직을 넣는 것이 좋음
+	// 데이터를 가지고 있는 쪽에서 비즈니스 로직을 갖고 있는 것이 좋음
+
+	/**
+	 * stock 증가
+	 */
+	public void addStock(int quantity) {
+		this.stockQuantity += quantity;
+	}
+
+	/**
+	 * stock 감소
+	 */
+	public void removeStock(int quantity) {
+		int restStock = this.stockQuantity - quantity;
+		if (restStock < 0) {
+			throw new NotEnoughStockException("need more stock");
+		}
+		this.stockQuantity = restStock;
+	}
 
 }
