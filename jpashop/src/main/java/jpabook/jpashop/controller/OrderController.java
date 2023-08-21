b512378f48947c961c4f839d6b15b1a2536445a8
ykmxxi <ykmxxi97@gmail.com>
2023-08-21 12:08:20 +0900

    Implement #11- order cancel feature
    - 주문 취소 기능

--- jpashop/src/main/java/jpabook/jpashop/controller/OrderController.java
+++ jpashop/src/main/java/jpabook/jpashop/controller/OrderController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -51,6 +52,12 @@ public class OrderController {
 		List<Order> orders = orderService.findOrders(orderSearch);
 		model.addAttribute("orders", orders);
 		return "order/orderList";
+	}
+
+	@PostMapping("/orders/{orderId}/cancel")
+	public String cancelOrder(@PathVariable("orderId") Long orderId) {
+		orderService.cancelOrder(orderId);
+		return "redirect:/orders";
 	}
 
 }
