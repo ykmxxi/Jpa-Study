37a5d1a49eaadb6d6250b9bc41af2a6ba3c54103
ykmxxi <ykmxxi97@gmail.com>
2023-08-25 11:46:47 +0900

    Add #17- instance field
    - 나이
    - 역할(회원 or 관리자)
    - 생성 날짜, 수정 날짜
    - 설명

--- ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -1,8 +1,16 @@
 package hellojpa;
 
+import java.util.Date;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.persistence.Transient;
 
 @Entity
 public class Member {
@@ -13,6 +21,23 @@ public class Member {
 	// 제약조건 추가: 회원 이름은 필수이고 10자를 초과하면 안됨
 	@Column(nullable = false, length = 10)
 	private String name;
+
+	private Integer age;
+
+	@Enumerated(EnumType.STRING)
+	private RoleType rolType;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date createdDate;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date lastModifiedDate;
+
+	@Lob
+	private String description;
+
+	@Transient
+	private int temp;
 
 	// JPA 스펙 상 기본 생성자가 필요
 	public Member() {
