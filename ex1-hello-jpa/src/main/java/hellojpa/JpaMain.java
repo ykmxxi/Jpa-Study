37548a42468a21cedfa13b6f2370c4bcd40cc560
ykmxxi <ykmxxi97@gmail.com>
2023-08-26 14:55:47 +0900

    Implement #19- example code
    - 저장
    - 참조를 이용한 조회
    - 참조를 이용한 수정

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -1,7 +1,5 @@
 package hellojpa;
 
-import java.util.List;
-
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
@@ -14,49 +12,31 @@ public class JpaMain {
 		EntityManager em = emf.createEntityManager();
 		EntityTransaction tx = em.getTransaction();
 
+		tx.begin();
 		try {
-			tx.begin(); // 트랜잭션 시작
+			// 팀 저장
+			Team team = new Team();
+			team.setName("TeamA");
+			em.persist(team);
 
-			// Member member = new Member();
-			// member.setId(2L);
-			// member.setName("HelloJPA");
-			// em.persist(member); // 저장
+			// 회원 저장
+			Member member = new Member();
+			member.setName("member1");
+			member.setTeam(team); // 단방향 연관관계 설정, 참조 저장
+			em.persist(member);
 
-			// 생성만 하면 member는 비영속 상태
-			Member member1 = new Member();
-			member1.setName("UserA");
-			Member member2 = new Member();
-			member2.setName("UserB");
-			Member member3 = new Member();
-			member3.setName("UserC");
+			// 조회
+			Member findMember = em.find(Member.class, member.getId());
 
-			// member가 영속 상태가 됨
-			// 이때 DB에 저장되는 것이 아님
-			em.persist(member1);
-			em.persist(member2);
-			em.persist(member3);
+			// 참조를 사용해서 연관관계 조회
+			Team findTeam = findMember.getTeam();
 
-			// IDENTITY 전략은 persist() 시점에 INSERT 쿼리를 날림
-			System.out.println("member1.id = " + member1.getId());
-			System.out.println("member2.id = " + member2.getId());
-			System.out.println("member3.id = " + member3.getId());
+			// 수정: 새로운 팀을 설정
+			Team teamB = new Team();
+			teamB.setName("TeamB");
+			em.persist(teamB);
 
-			// 변경 감지(Dirty Checking)
-			Member findMember = em.find(Member.class, 2L);
-			findMember.setName("이름 수정");
-
-			// 준영속(detached) 상태
-			Member findMember1 = em.find(Member.class, 1L);
-			findMember1.setName("준영속");
-
-			em.detach(findMember1);
-
-			List<Member> members = em.createQuery("select m from Member m", Member.class)
-				.getResultList();
-
-			for (Member m : members) {
-				System.out.println("member.name = " + m.getName());
-			}
+			member.setTeam(teamB);
 
 			tx.commit(); // 트랜잭션 커밋
 		} catch (Exception e) {
