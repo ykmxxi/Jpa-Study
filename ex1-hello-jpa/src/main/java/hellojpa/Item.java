89453303dc275ea8a3785e28725b1eea15bb953a
ykmxxi <ykmxxi97@gmail.com>
2023-08-30 10:34:11 +0900

    Implement #23- inheritance single table strategy
    - 싱글 테이블 전략
    - JPA 기본 전략
    - `@Inheritance` 어노테이션 생략 가능

--- ex1-hello-jpa/src/main/java/hellojpa/Item.java
+++ ex1-hello-jpa/src/main/java/hellojpa/Item.java
@@ -8,7 +8,7 @@ import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
 
 @Entity
-@Inheritance(strategy = InheritanceType.JOINED)
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @DiscriminatorColumn()
 public class Item {
 
