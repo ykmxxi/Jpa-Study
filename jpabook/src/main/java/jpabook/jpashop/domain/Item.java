9e00883621236996115ccace7e4d732b957d60cd
ykmxxi <ykmxxi97@gmail.com>
2023-08-30 14:33:17 +0900

    Implement #23- inheritance mapping by single table strategy
    - Item 엔티티를 상속 받는 엔티티 구현
    - 싱글 테이블 전략 사용

--- jpabook/src/main/java/jpabook/jpashop/domain/Item.java
+++ jpabook/src/main/java/jpabook/jpashop/domain/Item.java
@@ -4,12 +4,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.Column;
+import javax.persistence.DiscriminatorColumn;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
 import javax.persistence.ManyToMany;
 
 @Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn
 public class Item {
 
 	@Id @GeneratedValue
