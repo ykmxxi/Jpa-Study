74522965cb94fecb99ff0e6f9b3f413dbbb17d1b
ykmxxi <ykmxxi97@gmail.com>
2023-08-30 14:36:59 +0900

    Implement #23- mapped super class and applying
    - 전체 엔티티 공통 매핑 정보 클래스

--- jpabook/src/main/java/jpabook/jpashop/domain/Item.java
+++ jpabook/src/main/java/jpabook/jpashop/domain/Item.java
@@ -15,7 +15,7 @@ import javax.persistence.ManyToMany;
 @Entity
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @DiscriminatorColumn
-public class Item {
+public class Item extends BaseEntity {
 
 	@Id @GeneratedValue
 	@Column(name = "ITEM_ID")
