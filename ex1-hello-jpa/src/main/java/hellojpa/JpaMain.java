e310e7e4d34e8cccbb48546ef72656f17f5a9da8
ykmxxi <ykmxxi97@gmail.com>
2023-08-31 14:18:34 +0900

    Implement #25- transitive persistence by cascade
    - 영속성 전이
    - CascadeType.ALL 사용

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -15,23 +15,18 @@ public class JpaMain {
 		tx.begin();
 		try {
 
-			Member member = new Member();
-			member.setName("hello");
+			Child child1 = new Child();
+			Child child2 = new Child();
 
-			em.persist(member);
+			Parent parent = new Parent();
+			parent.addChild(child1);
+			parent.addChild(child2);
 
-			em.flush();
-			em.clear();
-
-			// 데이터베이스를 통해서 실제 엔티티 객체 조회
-//			Member findMember = em.find(Member.class, member.getId());
-//			System.out.println("findMember.Name = " + findMember.getName());
-
-			// 데이터베이스 조회를 미루는 가짜(프록시) 엔티티 객체 조회
-			Member referenceMember = em.getReference(Member.class, member.getId());
-			System.out.println("findMember = " + referenceMember.getClass()); // hellojpa.Member$HibernateProxy$baUfHhoA
-			System.out.println("findMember.id = " + referenceMember.getId()); // 이때 쿼리를 날리지 않음(값이 이미 있기 때문)
-			System.out.println("findMember.Name = " + referenceMember.getName()); // 이때 쿼리를 날림(실제 엔티티가 생성되어 있지 않고 DB에 값이 있기 때문)
+			// persist()를 3번 호출
+			// cascade = CascadeType.ALL 영속성 전이 설정 시 1번만 호출해도 모두 저장
+			em.persist(parent);
+//			em.persist(child1);
+//			em.persist(child2);
 
 			tx.commit(); // 트랜잭션 커밋
 		} catch (Exception e) {
