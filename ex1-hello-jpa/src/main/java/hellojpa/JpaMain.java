9ef482564be742147e1f57871a1319ecc4c0157f
ykmxxi <ykmxxi97@gmail.com>
2023-08-31 11:36:33 +0900

    Implement #25- proxy action mechanism example
    - 프록시 동작 메커니즘 예제

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -15,13 +15,23 @@ public class JpaMain {
 		tx.begin();
 		try {
 
-			Movie movie = new Movie();
-			movie.setName("아이언맨");
-			movie.setActor("로다주");
-			movie.setDirector("Marvel");
-			movie.setPrice(10000);
+			Member member = new Member();
+			member.setName("hello");
 
-			em.persist(movie);
+			em.persist(member);
+
+			em.flush();
+			em.clear();
+
+			// 데이터베이스를 통해서 실제 엔티티 객체 조회
+//			Member findMember = em.find(Member.class, member.getId());
+//			System.out.println("findMember.Name = " + findMember.getName());
+
+			// 데이터베이스 조회를 미루는 가짜(프록시) 엔티티 객체 조회
+			Member referenceMember = em.getReference(Member.class, member.getId());
+			System.out.println("findMember = " + referenceMember.getClass()); // hellojpa.Member$HibernateProxy$baUfHhoA
+			System.out.println("findMember.id = " + referenceMember.getId()); // 이때 쿼리를 날리지 않음(값이 이미 있기 때문)
+			System.out.println("findMember.Name = " + referenceMember.getName()); // 이때 쿼리를 날림(실제 엔티티가 생성되어 있지 않고 DB에 값이 있기 때문)
 
 			tx.commit(); // 트랜잭션 커밋
 		} catch (Exception e) {
