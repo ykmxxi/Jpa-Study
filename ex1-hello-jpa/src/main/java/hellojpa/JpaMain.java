2607fc8a14a05130289cc7332bc5868a33ce1560
ykmxxi <ykmxxi97@gmail.com>
2023-09-02 11:14:39 +0900

    Implement #27- reference share and copy example
    - 객체 참조 공유와 객체 복사 비교

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -17,12 +17,24 @@ public class JpaMain {
 		tx.begin();
 		try {
 
-			Member member = new Member();
-			member.setUsername("임베디드");
-			member.setHomeAddress(new Address("city", "street", "zipcode"));
-			member.setWorkPeriod(new Period(LocalDateTime.now(), LocalDateTime.now()));
+			Address address = new Address("city", "street", "10000");
 
-			em.persist(member);
+			Member member1 = new Member();
+			member1.setUsername("member1");
+			member1.setHomeAddress(address);
+			em.persist(member1);
+
+			// 객체를 공유하지 않고 복사해서 사용해야 한다
+			Address copyAddress = new Address(address.getCity(), address.getStreet(), address.getZipcode());
+			Member member2 = new Member();
+			member2.setUsername("member2");
+			member2.setHomeAddress(copyAddress);
+			em.persist(member2);
+
+			member1.getHomeAddress().setCity("newCity"); // member1의 city만 변경하고 싶음
+
+			// 복사해서 사용했기 때문에 member2는 변경되지 않음
+			System.out.println("member2.City() = " + member2.getHomeAddress().getCity());
 
 			tx.commit(); // 트랜잭션 커밋
 		} catch (Exception e) {
