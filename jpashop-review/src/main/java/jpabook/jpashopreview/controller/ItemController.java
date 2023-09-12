3df6cded771c2264670958a658be735b0d0076bf
ykmxxi <ykmxxi97@gmail.com>
2023-09-12 12:18:59 +0900

    Implement #33- item list view feature

--- jpashop-review/src/main/java/jpabook/jpashopreview/controller/ItemController.java
+++ jpashop-review/src/main/java/jpabook/jpashopreview/controller/ItemController.java
@@ -1,11 +1,14 @@
 package jpabook.jpashopreview.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import jpabook.jpashopreview.domain.item.Book;
+import jpabook.jpashopreview.domain.item.Item;
 import jpabook.jpashopreview.service.ItemService;
 import lombok.RequiredArgsConstructor;
 
@@ -34,4 +37,10 @@ public class ItemController {
 		return "redirect:/";
 	}
 
+	@GetMapping("/items")
+	public String list(Model model) {
+		List<Item> items = itemService.findItems();
+		model.addAttribute("items", items);
+		return "items/itemList";
+	}
 }
