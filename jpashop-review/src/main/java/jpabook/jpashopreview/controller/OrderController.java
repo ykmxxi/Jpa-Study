03f743050bdfbac15ef8d8cbf009031e1b158b5e
ykmxxi <ykmxxi97@gmail.com>
2023-09-13 10:59:09 +0900

    Implement #33- order list view feature

--- jpashop-review/src/main/java/jpabook/jpashopreview/controller/OrderController.java
+++ jpashop-review/src/main/java/jpabook/jpashopreview/controller/OrderController.java
@@ -5,10 +5,13 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import jpabook.jpashopreview.domain.Member;
+import jpabook.jpashopreview.domain.Order;
+import jpabook.jpashopreview.domain.OrderSearch;
 import jpabook.jpashopreview.domain.item.Item;
 import jpabook.jpashopreview.service.ItemService;
 import jpabook.jpashopreview.service.MemberService;
@@ -41,6 +44,14 @@ public class OrderController {
 
 		orderService.order(memberId, itemId, count);
 		return "redirect:/orders";
+	}
+
+	@GetMapping("/orders")
+	public String orderList(@ModelAttribute("orderSearch") OrderSearch orderSearch, Model model) {
+		List<Order> orders = orderService.findOrders(orderSearch);
+		model.addAttribute("orders", orders);
+
+		return "order/orderList";
 	}
 
 }
