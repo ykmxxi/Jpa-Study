28aec3d7faae053cafece12db80aab27b4717664
ykmxxi <ykmxxi97@gmail.com>
2023-09-14 11:02:34 +0900

    Implement #35- create member api v1
    - 엔티티를 Request Body에 직접 매핑
    - 실무에서 사용하면 안되는 방법

---
+++ jpashop/src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -0,0 +1,41 @@
+package jpabook.jpashop.api;
+
+import javax.validation.Valid;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.service.MemberService;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberApiController {
+
+	private final MemberService memberService;
+
+	/**
+	 * V1: 엔티티를 Request Body에 직접 매핑
+	 */
+	@PostMapping("/api/v1/members")
+	public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member) {
+		Long id = memberService.join(member);
+
+		return new CreateMemberResponse(id);
+	}
+
+	@Data
+	static class CreateMemberResponse {
+
+		private Long id;
+
+		public CreateMemberResponse(Long id) {
+			this.id = id;
+		}
+
+	}
+
+}
