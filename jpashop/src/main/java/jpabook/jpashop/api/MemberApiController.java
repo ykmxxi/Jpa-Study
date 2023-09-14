1bab28daa82eaa4d381ae7052f5c530ec2ed8313
ykmxxi <ykmxxi97@gmail.com>
2023-09-14 11:42:19 +0900

    Implement #35- update member api v2
    - POST 요청
    - 변경 감지(dirty checking) 기능 이용

--- jpashop/src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ jpashop/src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -2,12 +2,14 @@ package jpabook.jpashop.api;
 
 import javax.validation.Valid;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.service.MemberService;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 
@@ -39,6 +41,19 @@ public class MemberApiController {
 		return new CreateMemberResponse(id);
 	}
 
+	/**
+	 * 회원 수정
+	 */
+	@PostMapping("/api/v2/members/{id}")
+	public UpdateMemberResponse updateMemberV2(
+		@PathVariable("id") Long id,
+		@RequestBody @Valid UpdateMemberRequest request) {
+
+		memberService.update(id, request.getName());
+		Member findMember = memberService.findOne(id);
+		return new UpdateMemberResponse(findMember.getId(), findMember.getName());
+	}
+
 	@Data
 	static class CreateMemberResponse {
 
@@ -53,6 +68,22 @@ public class MemberApiController {
 	@Data
 	static class CreateMemberRequest {
 
+		private String name;
+
+	}
+
+	@Data
+	static class UpdateMemberRequest {
+
+		private String name;
+
+	}
+
+	@Data
+	@AllArgsConstructor
+	static class UpdateMemberResponse {
+
+		private Long id;
 		private String name;
 
 	}
