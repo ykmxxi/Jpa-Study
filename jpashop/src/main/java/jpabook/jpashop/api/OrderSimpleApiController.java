0e8fe4e62b36c231aa52f8cdb6a373a225807a25
ykmxxi <ykmxxi97@gmail.com>
2023-09-15 14:37:49 +0900

    Implement #37- simple order inquiry api v4
    - JPA에서 DTO로 바로 조회
    - 조회용 리포지토리 별도 생성
    - SELECT 절에서 원하는 데이터를 직접 선택하므로 DB -> 애플리케이션 네트워크 용량 최적화(미비함)
    - 리포지토리 재사용성이 떨어짐

--- jpashop/src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ jpashop/src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -12,6 +12,8 @@ import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.OrderSearch;
 import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
+import jpabook.jpashop.repository.order.simplequery.OrderSimpleQueryDto;
+import jpabook.jpashop.repository.order.simplequery.OrderSimpleQueryRepository;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 
@@ -26,6 +28,7 @@ import lombok.RequiredArgsConstructor;
 public class OrderSimpleApiController {
 
 	private final OrderRepository orderRepository;
+	private final OrderSimpleQueryRepository orderSimpleQueryRepository;
 
 	/**
 	 * V1. 엔티티 직접 노출
@@ -70,6 +73,16 @@ public class OrderSimpleApiController {
 		return orders.stream()
 					 .map(SimpleOrderDto::new)
 					 .collect(Collectors.toList());
+	}
+
+	/**
+	 * V4. JPA에서 DTO로 바로 조회
+	 * - 쿼리 1번 호출
+	 * - select 절에서 원하는 데이터만 선택해서 조회
+	 */
+	@GetMapping("/api/v4/simple-orders")
+	public List<OrderSimpleQueryDto> ordersV4() {
+		return orderSimpleQueryRepository.findOrderDtos();
 	}
 
 	@Data
