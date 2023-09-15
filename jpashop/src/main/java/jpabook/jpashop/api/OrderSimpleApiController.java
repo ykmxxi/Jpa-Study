7c71fa39838cc9d8cdeaca18112e975a435c25a5
ykmxxi <ykmxxi97@gmail.com>
2023-09-15 12:21:49 +0900

    Implement #37- simple order inquiry api v2
    - 엔티티를 조회해서 DTO로 변환(fetch join 사용X)
    - 지연로딩으로 쿼리 N번 호출(성능 문제)
    - 1 + N + N(모든 주문 조회 + 주문별 회원 조회 + 주문별 배송 조회)

--- jpashop/src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ jpashop/src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -1,13 +1,18 @@
 package jpabook.jpashop.api;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import jpabook.jpashop.domain.Address;
 import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.OrderSearch;
+import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 
 /**
@@ -36,6 +41,42 @@ public class OrderSimpleApiController {
 			order.getDelivery().getAddress(); // LAZY 강제 초기화
 		}
 		return all;
+	}
+
+	/**
+	 * V2. 엔티티를 조회해서 DTO로 변환(fetch join 사용X)
+	 * - 단점: 지연로딩으로 쿼리 N번 호출(성능 문제)
+	 * - 주문이 2개 이면
+	 * 	- SQL 1번: 주문 조회(2개)
+	 * 	- 1번째 주문: 회원 조회 + 배송 조회 -> SQL 2번
+	 * 	- 2번째 주문: 회원 조회 + 배송 조회 -> SQL 2번
+	 * 	- 총 5번의 쿼리가 나감
+	 */
+	@GetMapping("/api/v2/simple-orders")
+	public List<SimpleOrderDto> ordersV2() {
+		List<Order> orders = orderRepository.findAllByString(new OrderSearch());
+		return orders.stream()
+					 .map(SimpleOrderDto::new)
+					 .collect(Collectors.toList());
+	}
+
+	@Data
+	static class SimpleOrderDto {
+
+		private Long orderId;
+		private String name;
+		private LocalDateTime orderDate;
+		private OrderStatus orderStatus;
+		private Address address;
+
+		public SimpleOrderDto(Order order) {
+			orderId = order.getId();
+			name = order.getMember().getName(); // LAZY 초기화
+			orderDate = order.getOrderDate();
+			orderStatus = order.getStatus();
+			address = order.getDelivery().getAddress(); // LAZY 초기화
+		}
+
 	}
 
 }
