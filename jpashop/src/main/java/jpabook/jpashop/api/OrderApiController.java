48144febdde40eefb9dc3e2066f49f9e1db0eb5c
ykmxxi <ykmxxi97@gmail.com>
2023-09-16 10:48:16 +0900

    Implement #39- order inquiry api v2
    - 엔티티를 조회해서 DTO로 변환
    - fetch join 사용X
    - 너무 많은 SQL이 실행됨: 1 + N + N + N

--- jpashop/src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ jpashop/src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -1,14 +1,20 @@
 package jpabook.jpashop.api;
 
+import static java.util.stream.Collectors.toList;
+
+import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import jpabook.jpashop.domain.Address;
 import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.OrderItem;
 import jpabook.jpashop.domain.OrderSearch;
+import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 
 /**
@@ -48,6 +54,58 @@ public class OrderApiController {
 			orderItems.forEach(orderItem -> orderItem.getItem().getName());
 		}
 		return all;
+	}
+
+	/**
+	 * V2. 엔티티를 조회해서 DTO로 변환(fetch join 사용X)
+	 * - 트랜잭션 안에서 지연 로딩 필요
+	 */
+	@GetMapping("/api/v2/orders")
+	public List<OrderDto> ordersV2() {
+		List<Order> orders = orderRepository.findAllByString(new OrderSearch());
+
+		return orders.stream()
+					 .map(OrderDto::new)
+					 .collect(toList());
+	}
+
+	@Data
+	static class OrderDto {
+
+		private Long orderId;
+		private String name;
+		private LocalDateTime orderDate;
+		private OrderStatus orderStatus;
+		private Address address;
+		private List<OrderItemDto> orderItems;
+
+		public OrderDto(Order order) {
+			orderId = order.getId();
+			name = order.getMember().getName();
+			orderDate = order.getOrderDate();
+			orderStatus = order.getStatus();
+			address = order.getDelivery().getAddress();
+			orderItems = order.getOrderItems()
+							  .stream()
+							  .map(OrderItemDto::new)
+							  .collect(toList());
+		}
+
+	}
+
+	@Data
+	static class OrderItemDto {
+
+		private String itemName;
+		private int orderPrice;
+		private int count;
+
+		public OrderItemDto(OrderItem orderItem) {
+			itemName = orderItem.getItem().getName();
+			orderPrice = orderItem.getOrderPrice();
+			count = orderItem.getCount();
+		}
+
 	}
 
 }
