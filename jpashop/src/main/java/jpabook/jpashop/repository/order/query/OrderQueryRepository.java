b0719fa3c6322d722dcc75fcde63b9aaa81f602e
ykmxxi <ykmxxi97@gmail.com>
2023-09-18 20:31:52 +0900

    Implement #39- order inquiry api v5
    - JPA에서 DTO로 바로 조회
    - 컬렉션 1 조회 최적화 버전 (1 + 1Query)
    - V4 최적화, MAP 사용
    - 페이징 가능
    -  데이터를 한꺼번에 처리할 때 많이 사용하는 방식
    - Query: 루트 1번, 컬렉션 1번

--- jpashop/src/main/java/jpabook/jpashop/repository/order/query/OrderQueryRepository.java
+++ jpashop/src/main/java/jpabook/jpashop/repository/order/query/OrderQueryRepository.java
@@ -1,6 +1,8 @@
 package jpabook.jpashop.repository.order.query;
 
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import javax.persistence.EntityManager;
 
@@ -30,6 +32,20 @@ public class OrderQueryRepository {
 	}
 
 	/**
+	 * 최적화
+	 * Query: 루트 1번, 컬렉션 1번
+	 * 데이터를 한꺼번에 처리할 때 많이 사용하는 방식
+	 */
+	public List<OrderQueryDto> findAllByDto_optimization() {
+		List<OrderQueryDto> result = findOrders();
+
+		Map<Long, List<OrderItemQueryDto>> orderItemMap = findOrderItemMap(toOrderIds(result));
+
+		result.forEach(o -> o.setOrderItems(orderItemMap.get(o.getOrderId())));
+		return result;
+	}
+
+	/**
 	 * 1:N 관계(컬렉션)를 제외한 나머지를 한번에 조회
 	 */
 	private List<OrderQueryDto> findOrders() {
@@ -54,6 +70,27 @@ public class OrderQueryRepository {
 				 .setParameter("orderId", orderId)
 				 .getResultList();
 
+	}
+
+	private List<Long> toOrderIds(List<OrderQueryDto> result) {
+		return result.stream()
+					 .map(OrderQueryDto::getOrderId)
+					 .collect(Collectors.toList());
+	}
+
+	private Map<Long, List<OrderItemQueryDto>> findOrderItemMap(List<Long> orderIds) {
+		// order_item과 item을 inner join 하고, in 쿼리가 나감
+		List<OrderItemQueryDto> orderItems =
+			em.createQuery(
+				  "select new jpabook.jpashop.repository.order.query.OrderItemQueryDto(oi.order.id, i.name, oi.orderPrice, oi.count)" +
+					  " from OrderItem oi" +
+					  " join oi.item i" +
+					  " where oi.order.id in :orderIds", OrderItemQueryDto.class)
+			  .setParameter("orderIds", orderIds)
+			  .getResultList();
+
+		return orderItems.stream()
+						 .collect(Collectors.groupingBy(OrderItemQueryDto::getOrderId));
 	}
 
 }
