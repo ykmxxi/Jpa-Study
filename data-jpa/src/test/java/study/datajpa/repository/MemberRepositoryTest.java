1be1f552503b28b9a7e5fae32099e83e51c265ab
ykmxxi <ykmxxi97@gmail.com>
2023-09-24 11:54:21 +0900

    Implement #43- spring-data-jpa execution test

---
+++ data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -0,0 +1,35 @@
+package study.datajpa.repository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import study.datajpa.entity.Member;
+
+@SpringBootTest
+@Transactional
+@Rollback(false)
+class MemberRepositoryTest {
+
+	@Autowired private MemberRepository memberRepository;
+
+	@Test
+	void testMember() {
+		// given
+		Member member = new Member("memberA");
+		Member savedMember = memberRepository.save(member);
+
+		// when
+		Member findMember = memberRepository.findById(savedMember.getId()).get();
+
+		// then
+		assertThat(findMember.getId()).isEqualTo(member.getId());
+		assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+		assertThat(findMember).isEqualTo(member);
+	}
+
+}
