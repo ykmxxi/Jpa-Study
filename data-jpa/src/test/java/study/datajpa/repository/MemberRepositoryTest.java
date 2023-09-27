4c60f33ba408934219dbafb13fef892f65731af3
ykmxxi <ykmxxi97@gmail.com>
2023-09-27 11:27:24 +0900

    Implement #47- fetch join by entitygraph annotation
    - `@EntityGraph`로 페치 조인

--- data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -292,4 +292,33 @@ class MemberRepositoryTest {
 		assertThat(isLazy2).isTrue();
 	}
 
+	@Test
+	void entityGraph() {
+		// given
+		Team teamA = new Team("teamA");
+		Team teamB = new Team("teamB");
+		teamRepository.save(teamA);
+		teamRepository.save(teamB);
+
+		Member member1 = new Member("member1", 10, teamA);
+		Member member2 = new Member("member2", 20, teamB);
+		memberRepository.save(member1);
+		memberRepository.save(member2);
+
+		em.flush();
+		em.clear();
+
+		// when
+		List<Member> members = memberRepository.findAll();
+
+		// then
+		for (Member member : members) {
+			System.out.println("member = " + member);
+			System.out.println("member.team = " + member.getTeam().getName());
+		}
+
+		boolean isLazy1 = Hibernate.isInitialized(members.get(0).getTeam());
+		assertThat(isLazy1).isTrue();
+	}
+
 }
