407db8a9d75b232bc5957b8bc69ab818307562b2
ykmxxi <ykmxxi97@gmail.com>
2023-09-28 14:21:11 +0900

    Implement #49- paging and sort by spring-data-jpa

--- data-jpa/src/main/java/study/datajpa/controller/MemberController.java
+++ data-jpa/src/main/java/study/datajpa/controller/MemberController.java
@@ -2,11 +2,15 @@ package study.datajpa.controller;
 
 import javax.annotation.PostConstruct;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
+import study.datajpa.dto.MemberDto;
 import study.datajpa.entity.Member;
 import study.datajpa.repository.MemberRepository;
 
@@ -33,9 +37,21 @@ public class MemberController {
 		return member.getUsername();
 	}
 
+	@GetMapping("/members")
+	public Page<MemberDto> list(@PageableDefault(size = 5) Pageable pageable) {
+		// Page<Member> page = memberRepository.findAll(pageable);
+		// Page<MemberDto> dtoPage = page.map(MemberDto::new);
+		// return dtoPage;
+		return memberRepository.findAll(pageable)
+							   .map(MemberDto::new);
+
+	}
+
 	@PostConstruct
 	public void init() {
-		memberRepository.save(new Member("userA"));
+		for (int i = 0; i < 100; i++) {
+			memberRepository.save(new Member("user" + i, i));
+		}
 	}
 
 }
