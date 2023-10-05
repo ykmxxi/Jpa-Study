81a896fad78e74740a2da73f1a20e4df256d4dc5
ykmxxi <ykmxxi97@gmail.com>
2023-10-05 12:20:46 +0900

    Test #55- constant and string concat
    - 상수
    - 문자열 연결: 문자가 아닌 다른 타입들은 stringValue()로 변환해 연결

--- querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
+++ querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -20,6 +20,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.querydsl.core.QueryResults;
 import com.querydsl.core.Tuple;
 import com.querydsl.core.types.dsl.CaseBuilder;
+import com.querydsl.core.types.dsl.Expressions;
 import com.querydsl.core.types.dsl.NumberExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 
@@ -491,6 +492,31 @@ public class QuerydslBasicTest {
 			Integer age = tuple.get(member.age);
 			Integer rank = tuple.get(rankPath);
 			System.out.println("username = " + username + " age = " + age + " rank = " + rank);
+		}
+	}
+
+	@Test
+	void constant() {
+		List<Tuple> result = queryFactory
+			.select(member.username, Expressions.constant("A"))
+			.from(member)
+			.fetch();
+
+		for (Tuple tuple : result) {
+			System.out.println("tuple = " + tuple);
+		}
+	}
+
+	@Test
+	void concat() {
+		List<String> result = queryFactory
+			.select(member.username.concat("_").concat(member.age.stringValue()))
+			.from(member)
+			.where(member.username.eq("member1"))
+			.fetch();
+
+		for (String s : result) {
+			System.out.println("s = " + s);
 		}
 	}
 
