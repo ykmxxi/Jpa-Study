aafa9782ad3ecc791b8491d3d5b712cf53572a03
ykmxxi <ykmxxi97@gmail.com>
2023-10-06 11:16:01 +0900

    Test #57- when alias different

--- querydsl/src/test/java/study/querydsl/QuerydslIntermediateTest.java
+++ querydsl/src/test/java/study/querydsl/QuerydslIntermediateTest.java
@@ -13,11 +13,15 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.querydsl.core.Tuple;
+import com.querydsl.core.types.ExpressionUtils;
 import com.querydsl.core.types.Projections;
+import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 
 import study.querydsl.dto.MemberDto;
+import study.querydsl.dto.UserDto;
 import study.querydsl.entity.Member;
+import study.querydsl.entity.QMember;
 import study.querydsl.entity.Team;
 
 @SpringBootTest
@@ -134,6 +138,29 @@ public class QuerydslIntermediateTest {
 
 		for (MemberDto memberDto : result) {
 			System.out.println("memberDto = " + memberDto);
+		}
+	}
+
+	/**
+	 * 별칭(alias)이 다를 때
+	 */
+	@Test
+	void findUserDtoByField() {
+		QMember memberSub = new QMember("memberSub");
+		List<UserDto> result = queryFactory
+			.select(
+				Projections.fields(
+					UserDto.class,
+					member.username.as("name"),
+					ExpressionUtils.as(JPAExpressions
+										   .select(memberSub.age.max())
+										   .from(memberSub), "age")
+				))
+			.from(member)
+			.fetch();
+
+		for (UserDto userDto : result) {
+			System.out.println("userDto = " + userDto);
 		}
 	}
 
