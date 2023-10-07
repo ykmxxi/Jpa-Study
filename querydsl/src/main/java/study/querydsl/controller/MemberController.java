a3032598b5eeb51e10b4158586239f6f459da2a1
ykmxxi <ykmxxi97@gmail.com>
2023-10-07 12:25:32 +0900

    Implement #59- search member controller
    - 샘플 데이터 추가
    - 조회 API 컨트롤러

---
+++ querydsl/src/main/java/study/querydsl/controller/MemberController.java
@@ -0,0 +1,24 @@
+package study.querydsl.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+import study.querydsl.dto.MemberSearchCondition;
+import study.querydsl.dto.MemberTeamDto;
+import study.querydsl.repository.MemberJpaRepository;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberController {
+
+	private final MemberJpaRepository memberJpaRepository;
+
+	@GetMapping("/v1/members")
+	public List<MemberTeamDto> searchMemberV1(MemberSearchCondition condition) {
+		return memberJpaRepository.search(condition);
+	}
+
+}
