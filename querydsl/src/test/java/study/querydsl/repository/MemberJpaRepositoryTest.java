480ca1007e8dc50e991743b98cf3442b984b6efe
ykmxxi <ykmxxi97@gmail.com>
2023-10-07 11:30:12 +0900

    Implement #59- dynamic query with boolean builder
    - 검색 조건 DTO 구현
    - 조회용 DTO 구현: `@QueryProjection` 사용
    - BooleanBuilder 사용

--- querydsl/src/test/java/study/querydsl/repository/MemberJpaRepositoryTest.java
+++ querydsl/src/test/java/study/querydsl/repository/MemberJpaRepositoryTest.java
@@ -11,7 +11,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
+import study.querydsl.dto.MemberSearchCondition;
+import study.querydsl.dto.MemberTeamDto;
 import study.querydsl.entity.Member;
+import study.querydsl.entity.Team;
 
 @SpringBootTest
 @Transactional
@@ -50,6 +53,39 @@ class MemberJpaRepositoryTest {
 		// 조건 검색: querydsl
 		List<Member> result2 = memberJpaRepository.findByUsername_Querydsl("member1");
 		assertThat(result2).containsExactly(member);
+	}
+
+	@Test
+	void searchTest() {
+		Team teamA = new Team("teamA");
+		Team teamB = new Team("teamB");
+		em.persist(teamA);
+		em.persist(teamB);
+
+		Member member1 = new Member("member1", 10, teamA);
+		Member member2 = new Member("member2", 20, teamA);
+		Member member3 = new Member("member3", 30, teamB);
+		Member member4 = new Member("member4", 40, teamB);
+		em.persist(member1);
+		em.persist(member2);
+		em.persist(member3);
+		em.persist(member4);
+
+		MemberSearchCondition condition1 = new MemberSearchCondition();
+		condition1.setAgeGoe(35);
+		condition1.setAgeLoe(40);
+		condition1.setTeamName("teamB");
+
+		MemberSearchCondition condition2 = new MemberSearchCondition();
+		condition2.setTeamName("teamB");
+
+		List<MemberTeamDto> result1 = memberJpaRepository.searchByBuilder(condition1);
+		List<MemberTeamDto> result2 = memberJpaRepository.searchByBuilder(condition2);
+
+		assertThat(result1).extracting("username")
+						   .containsExactly("member4");
+		assertThat(result2).extracting("username")
+						   .containsExactly("member3", "member4");
 	}
 
 }
